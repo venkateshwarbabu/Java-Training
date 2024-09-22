4aa2de71236e91dea3116acc6c21b8a2133b5081
venkateshwarbabu <venkateshwarbabupitta@gmail.com>
2024-09-22 23:46:08 +0530

    from workspace new java file

---
+++ SampleProject/src/com/Java8/Basic.java
@@ -0,0 +1,11 @@
+package com.Java8;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Wellcome to java 1.8");
+
+	}
+
+}
